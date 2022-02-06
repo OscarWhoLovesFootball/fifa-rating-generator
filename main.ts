23f6820c9e6ea47050e571b39cfad63201ec9236
OscarWhoLovesFootball <99141273+OscarWhoLovesFootball@users.noreply.github.com>
2022-02-06 18:45:43 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(randint(1, 99))
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showString("Made By Oscar")
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
